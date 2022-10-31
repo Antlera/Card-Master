6951f35907a61525a50f7ac47aecd03ea977166c
Antlers <3219599757@qq.com>
2022-10-31 22:20:00 +0800

    Modify the topic of mqtt.

---
+++ Src/arduino_secrets.h
@@ -0,0 +1,16 @@
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+const char *ssid = "360WiFi-E8DF1C";
+const char *password = "601zuiwudi";
+const char *mqttServer = "iot.ranye-iot.net";
+
+
+// ****************************************************
+const char *mqttUserName = "antlers_2";   // 服务端连接用户名
+const char *mqttPassword = "ryry0101";    // 服务端连接密码
+const char *clientId = "antlers_2_id";    // 客户端id 
+const char *subTopic = "antlers/nodered/mysql"; // 订阅主题
+const char *pubTopic = "antlers/nodered/mysql"; // 订阅主题
+const char *willTopic = "antlers/lastwill"; // 遗嘱主题名称
+// ****************************************************
+#endif
